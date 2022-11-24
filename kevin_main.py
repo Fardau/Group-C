d34d98ff8b2b536527b7c8d694d2cf393745dfec
fvanslooten <fvanslooten@deloitte.nl>
2022-11-24 15:14:25 +0100

    url

--- kevin_main.py
+++ kevin_main.py
@@ -11,3 +11,8 @@ def abc(a):
 #Functionality 2: Make function
 #Functionality 3: Get the data from API
 #Functionality 4: Performance
+
+
+
+
+response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
