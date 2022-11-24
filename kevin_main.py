6bcfed00d6e6ad6632717f39997ce196869f7804
Lucie <lmohylova@deloitte.nl>
2022-11-24 14:46:29 +0100

    Start

--- kevin_main.py
+++ kevin_main.py
@@ -1,12 +1,21 @@
-print("hello")
-print("this is a test #2")
-
-abc(1)
-print("testtt")
-
 def abc(a):
     return a+1
 
+# The INVESTMENT game
+
+# Import packages
+
+
+# Get the data from API
+
+
+# Set up a user
+
+
+# Performance
+
+
+# TASKS:
 #Functionality 1: Setup user
 #Functionality 2: Make function
 #Functionality 3: Get the data from API
