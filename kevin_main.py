d723f665ec41913dffbcfc4d930c6b4d7a11bb7f
kehalim123 <kevinhalim@gmail.com>
2022-11-24 15:26:59 +0100

    kevin_commit_15:26

--- kevin_main.py
+++ kevin_main.py
@@ -5,6 +5,8 @@
 
 # Get the data from API
 
+response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
+
 
 # Set up a user
 
@@ -14,6 +16,31 @@
 
 # TASKS:
 #Functionality 1: Setup user
+
+
+Users = [
+        {"username" : "Kevin",
+        "balance"   : 1,
+        "stocks"    : []},
+        {"username" : "Lucie",
+        "balance"   : 1,
+        "stocks"    : []},
+        {"username" : "Fardau",
+         "balance"  : 1,
+         "stocks"   : []},
+]
+def userSetup():
+    userx = input("EnterUsername")
+#    userxpw = input("EnterPW")
+    if userx is userx:
+        print("Welcome","", userx)
+        #print("Your balance today is", Users(balance)) # To ask RJ
+    else:
+        print("unknown user")
+    return
+print(userSetup())
+
+
 #Functionality 2: Make function
 #Functionality 3: Get the data from API
 #Functionality 4: Performance
@@ -21,4 +48,4 @@
 
 
 
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
+
